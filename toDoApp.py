2683fdb5f6e3b2733b78fec957f6d78613d42aa1
AJ <ajbsalva05@gmail.com>
2025-10-04 14:32:04 +0800

    Updated the .py file (REAL)

--- toDoApp.py
+++ toDoApp.py
@@ -2,38 +2,38 @@
 
 tasks=[]
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
+def addTask(task):
+    tasks.append(task)
+    print("Task added!")
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def showTasks():
+    if len(tasks)==0:
+        print("No tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range (len(tasks)):
+            print(i+1,".",tasks[i])
 
-def removetask(tasknumber):
+def removeTask(tasknumber):
     tasks.pop(tasknumber) 
-    print("task removed!!")
+    print("Task Removed!!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
-            addtask(t)
-        elif ch=="2":
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
+        choice = input("Enter choice: ")
+        if choice =="1":
+            task = input("Enter Task: ")
+            addTask(task)
+        elif choice == "2":
             showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        elif choice == "3":
+            listNumber = int(input("Enter task number to remove: "))
+            removeTask(listNumber)   
+        elif choice=="4":
+            break
         else:
-            print("wrong choice!!")
+            print("Wrong choice!!")
 main()
